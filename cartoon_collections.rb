8a27ea12d2ffe76847d6c8a44f00201742ae892d
friendly-format-0074 <samuelagasi@gmail.com>
2021-02-06 22:51:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,6 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  array.each {|char|
+  puts "#{array.length}. #{char}"}
 end
